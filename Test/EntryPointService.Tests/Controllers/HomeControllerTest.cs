6d7b62775070916917e56a8e9e3b373e544f554e
ShaoJun <music1913@gmail.com>
2016-09-23 14:34:04 +0800

    comment out some failure build code in unit test project.

--- Test/EntryPointService.Tests/Controllers/HomeControllerTest.cs
+++ Test/EntryPointService.Tests/Controllers/HomeControllerTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EntryPointService;
 using EntryPointService.Controllers;
 
